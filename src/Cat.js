27dea244339ab374ea8329c7380dce7877e114ce
Carlyse Jordan <carlyse.jordan@Carlyses-MacBook-Air.local>
2017-03-20 16:32:15 -0400

    on this log, I logged out the name and date of birth

--- src/Cat.js
+++ src/Cat.js
@@ -2,9 +2,16 @@
   'use strict';
 
   window.zoo = window.zoo || {};
+  let Animal = window.zoo.Animal;
 
-    let me = new window.zoo.Animal('lion');
-    console.log(me.name);
-    console.log('its a date!');
+  window.zoo.Cat = class Cat extends Animal {
+    constructor(name, dateOfBirth){
+      super(name, dateOfBirth);
+    }
+
+  };
+
+    let me = new window.zoo.Cat('lion', '4/22/1961');
+    console.log(me.name, me.dateOfBirth);
 
 }());
